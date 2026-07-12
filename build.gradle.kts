8f4b487905c1ab26168245c75cde65ff154acdc8
DancingSnow <1121149616@qq.com>
2026-07-12 14:33:21 +0800

    test: configure pure JUnit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
 
+import org.gradle.api.tasks.testing.Test
+
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
---
+++ src/test/java/cn/dancingsnow/appeu/TestHarnessTest.java
@@ -0,0 +1,13 @@
+package cn.dancingsnow.appeu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class TestHarnessTest {
+
+    @Test
+    void runsWithoutBootstrappingMinecraft() {
+        assertEquals(2, 1 + 1);
+    }
+}
